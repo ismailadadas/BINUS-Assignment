4d4bb545711215255d3d1a028125bbba3a1a1982
ismailadadas <ismailadadas@gmail.com>
2022-09-11 12:10:08 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[]args)
-        {
-            String namaA, namaB;
-            int umurA, umurB;
-            Scanner input = new Scanner(System.in);
-            Random random = new Random();
+    {
+        String namaA, namaB;
+        int umurA, umurB;
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
 
         System.out.println("Selamat Datang Di Program Ramalan Cupu");
         System.out.println("++++++++++++++++++++++++++++++++++++++");
         System.out.println('\n');
 
         System.out.println("Data Anda");
-        System.out.println("+++++++++++++++++++++++");
+        System.out.println("❤❤❤❤❤❤❤❤❤");
         System.out.print("Masukan Nama Anda");
         namaA = input.nextLine();
         System.out.print("Masukan Umur Anda");
@@ -24,18 +24,17 @@ public class Main {
         System.out.println();
 
         System.out.println("Data Pasangan Anda");
-        System.out.println("+++++++++++++++++++++++++++");
+        System.out.println("❤❤❤❤❤❤❤❤❤");
         System.out.print("Masukan Nama Pasangan Anda");
         namaB = input.nextLine();
         System.out.print("Masukan Umur Pasangan Anda");
         umurB = input.nextInt();
         System.out.println();
-        System.out.println("Tekan ENTER Untuk Melihat Hasil Ramalan");
-        breakConsule();
+        System.out.println("Tekan ENTER Untuk Melihat Hasil Ramalan...");
 
         System.out.println(namaA + " [ " + umurA + " ] Tahun ");
         System.out.println();
-            final int size = 3;
+        final int size = 3;
         for (int m = 0 ; m < size; m++) {
             for (int n = 0; n < 5 * size; n++) {
                 double post1 = Math.sqrt(Math.pow(m - size, 2) + Math.pow(n - size,2 ));
@@ -53,21 +52,21 @@ public class Main {
             for (int n = 0; n < m; n++){
                 System.out.print(' ');
             }
-        for (int n = 0; n <4 * size + 1 - 2 * m; n++) {
+            for (int n = 0; n <4 * size + 1 - 2 * m; n++) {
                 System.out.print("❤");
-        }
-        System.out.print(System.lineSeparator());
+            }
+            System.out.print(System.lineSeparator());
         }
 
         System.out.println();
         System.out.println(namaB + " ["+ umurB +"] Tahun ");
         System.out.println();
-            double value  = random.nextDouble() * 100;
-            while (value < 50);
-            value = random.nextDouble() * 100;
-            System.out.printf("Kecocokan anda dengan pasangan anda adalah : %.2f\n" , value, "%");
-            System.out.println("Terimakasih telah menggunakan aplikasi kami");
-            input.close();
+        double value  = random.nextDouble() * 100;
+        while (value < 50);
+        value = random.nextDouble() * 100;
+        System.out.printf("Kecocokan anda dengan pasangan anda adalah : %.2f\n" , value, "%");
+        System.out.println("Terima kasih karena telah menggunakan jasa Ramalan kami..");
+        input.close();
     }
     private static void breakConsule() {
         try {
